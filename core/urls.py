0595ad8d5ccb04c7647fbc002380dbf9dc58c6b4
Kevin López <kevinlopez2693@gmail.com>
2025-03-15 19:06:55 -0500

    disable authentication for Swagger API documentation

--- core/urls.py
+++ core/urls.py
@@ -67,7 +67,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
+    authentication_classes=[]  # Add this line to explicitly disable authentication for Swagger
 )
 
 auth_urlpatterns = [
